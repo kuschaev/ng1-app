11e01aaf6109a397250f213422bb1a9ac223ce9d
Nikolai <kuschaev@users.noreply.github.com>
2017-10-09 02:46:38 +0300

    map loading hotfix
    Added fixed size to ng-map.
    Need to fix map loading after switching between apps properly.

--- src/app/maps/maps.controller.js
+++ src/app/maps/maps.controller.js
@@ -4,7 +4,7 @@ export class MController {
     this._mdSidenav = $mdSidenav;
     console.log('MController');
     console.log(NgMap);
-    this.address = 'Kyiv';
+    this.address = 'Troieshchyna';
     NgMap.getMap().then((map) => {
       this.map = map;
       console.log(map.getCenter());
